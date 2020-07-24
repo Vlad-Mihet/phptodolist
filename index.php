cf8476cbc1af1800b8dda5b5749ca785950fb798
Vlad Mihet <61595636+Vlad-Mihet@users.noreply.github.com>
2020-07-24 22:38:31 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -15,41 +15,60 @@
   />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
   <link rel="stylesheet" href="./styles/style.css" />
-  <title>Todo List PHP & MySQL </title>
+  <title> Task Manager </title>
 </head>
 <body>
 <div class="container">
-<h1> To do list </h1>
-<form class="d-flex justify-content-between" action="index.php?add_todo" method="post">
-  <input id="addtodo" type="text" name="addtodo" placeholder="Add todo">
-  <input id="submit" type="submit" value="+" />
-</form>  
-<?php
+  <div class="task-container">
+    <div class="header-container">
+      <h1> Task Manager </h1>
+    </div>
 
-  $url =$_SERVER['REQUEST_URI'];
-  if (strpos($url, '?')) {
-    $x = explode('?', $url);
-    $sql = "DELETE FROM todos WHERE `item_id` = $x[1]";
-    mysqli_query($conn, $sql);
-  }
-  if (empty($_POST['addtodo']) === false) {
-    $todo = $_POST['addtodo'];
-    if (empty($x[1]) === false && $x[1] == 'add_todo') {
-      $sql = "INSERT INTO todos (`content`) VALUE ('$todo')";
-      mysqli_query($conn, $sql);
-    }
-  }
-  $sql = "SELECT * FROM todos; ";
-  $results = mysqli_query($conn, $sql);
-  $results_check = mysqli_num_rows($results);
-  if ($results_check > 0) {
-    while ($row = mysqli_fetch_assoc($results)) {
-      echo 
-        "<div class='list-item d-flex justify-content-between'>" . "<span class='item-title'>" . $row['content'] . "</span>" . "<a href='./index.php?{$row['item_id']}'> <i class='fas fa-trash-alt'> </i> </a>" . "</div>";
-    }
-  }
-?>
-</div>
+    <?php
+
+      $url =$_SERVER['REQUEST_URI'];
+      if ((int)strpos($url, '?')) {
+        $x = explode('?', $url);
+      
+        if (empty($_POST['addtodo']) === false) {
+          $todo = $_POST['addtodo'];
+        } 
+        if ($x[1] == 'add_todo') {
+          $sql = "INSERT INTO todos (`content`) VALUE ('$todo')";
+          mysqli_query($conn, $sql);
+          header('Location: '.'index.php');
+        } else if ((int)strpos($url, '?')) {
+          $x = explode('?', $url);
+          $sql = "DELETE FROM todos WHERE `item_id` = $x[1]";
+          mysqli_query($conn, $sql);
+          header('index.php');
+          header('Location: '.'index.php');
+        }
+      }
+      
+      unset($_POST['addtodo']);
+      unset($todo);
+
+      $sql = "SELECT * FROM todos; ";
+      $results = mysqli_query($conn, $sql);
+      $results_check = mysqli_num_rows($results);
+      if ($results_check > 0) {
+        while ($row = mysqli_fetch_assoc($results)) {
+          $time = substr($row['creation_date'], 11, 5);
+          echo "<div class='list-item d-flex justify-content-between'>" . "<div class='title-time'>" . "<span class='item-title'>" . $row['content'] . "</span>" . "<span class='time'>" . "Created at: " . $time . "</span>" . "</div>" . "<a href='./index.php?{$row['item_id']}'> <i class='fas fa-trash-alt fa-lg'> </i> </a>" . "</div>";
+        }
+      }
+    ?>
+
+    <div class="form-container">
+      <form class="d-flex justify-content-between" action="index.php?add_todo" method="post">
+        <input id="addtodo" type="text" name="addtodo" placeholder="Add todo">
+        <button class="submit-button" type="submit" name="submit"> <i class="fas fa-plus"></i> </button>
+      </form>  
+    </div>
+
+    </div>
+  </div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
